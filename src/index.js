39409f93ce55dfa94c67135c75689ea9caba325c
laisalamir <lmslamir@gmail.com>
2021-07-18 14:33:11 +0100

    atualizações

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
 
